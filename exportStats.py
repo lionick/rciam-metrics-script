d25029a58503972b58068b1fee9b12e22480124f
Nick Mastoris <nmastoris@cti.gr>
2021-03-22 09:35:40 +0200

    make timestamp as integer

--- exportStats.py
+++ exportStats.py
@@ -3,10 +3,10 @@ from Services.proxystatisticsService import proxystatisticsService
 import time
 
 users = comanageService.getRegisteredUsers()
-registeredUsersMetric = "aai_registered_users_total {0} {1}".format(users, time.time())
+registeredUsersMetric = "aai_registered_users_total {0} {1}".format(users, int(time.tme()))
 logins = proxystatisticsService.getUsersLogins()
-totalLoginsMetric = "aai_logins_total {0} {1}".format(logins, time.time())
-f = open("aai_stats.txt", "w")
+totalLoginsMetric = "aai_logins_total {0} {1}".format(logins, int(time.time()))
+f = open("aai_metrics.txt", "w")
 f.write("{0}\n".format(registeredUsersMetric))
 f.write("{0}\n".format(totalLoginsMetric))
 f.close()
