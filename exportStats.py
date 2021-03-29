5b1b5ff096f4ae185f0e95d863c069cfb5e61cfe
Nick Mastoris <nmastoris@cti.gr>
2021-03-29 13:56:30 +0300

    minor fixes

--- exportStats.py
+++ exportStats.py
@@ -1,16 +1,19 @@
 from Services.comanageService import comanageService
 from Services.proxystatisticsService import proxystatisticsService
+from Services.nginxlogsService import nginxlogsService
 import time
 
 users = comanageService.getRegisteredUsers()
 registeredUsersMetric = "aai_registered_users_total {0} {1}".format(users, int(time.time()))
 logins = proxystatisticsService.getUsersLogins()
 totalLoginsMetric = "aai_logins_total {0} {1}".format(logins, int(time.time()))
+api_requests = nginxlogsService.getApiRequests()
+totalApiRequestsMetric = " aai_api_requests_total {0} {1}".format(logins, int(time.time()))
 f = open("aai_metrics.txt", "w")
 f.write("# TYPE aai_registered_users_total gauge\n")
 f.write("{0}\n".format(registeredUsersMetric))
 f.write("# TYPE aai_logins_total counter\n")
 f.write("{0}\n".format(totalLoginsMetric))
 f.write("# TYPE aai_api_requests_total counter\n")
-f.write("{0}\n".format(totalLoginsMetric))
+f.write("{0}\n".format(totalApiRequestsMetric))
 f.close()
