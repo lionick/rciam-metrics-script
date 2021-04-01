eea9e67ede0c817fd7a0975fb7abc34a2f126acb
Nick Mastoris <nmastoris@cti.gr>
2021-04-01 10:45:00 +0300

    add table for syslogs

--- config-templates/pgsql_tables.sql
+++ config-templates/pgsql_tables.sql
@@ -1,4 +1,11 @@
--- AII metrics table
+-- syslogs table
+CREATE TABLE IF NOT EXISTS syslogs (
+    id SERIAL PRIMARY KEY,
+    log_message character varying(255) NOT NULL,
+    created timestamptz NOT NULL
+);
+
+-- aii metrics table
 CREATE TABLE IF NOT EXISTS aai_metrics (
     id SERIAL PRIMARY KEY,
     metric_name character varying(255) NOT NULL,
