b629b7fa0f3828216bacf1b1fc7e9cf913e4ac9a
Nick Mastoris <nmastoris@cti.gr>
2021-04-05 10:34:55 +0300

    run script from everywhere

--- exportStats.py
+++ exportStats.py
@@ -1,17 +1,23 @@
+#!/usr/local/bin/python3
+import os
+import sys
+# change working directory
+os.chdir(os.path.dirname(os.path.abspath(sys.argv[0])))
+
 from Services.comanageService import comanageService
 from Services.proxystatisticsService import proxystatisticsService
 from Services.nginxlogsService import nginxlogsService
 import time
 
-users = comanageService.getRegisteredUsers()
-registeredUsersMetric = "aai_registered_users_total {0} {1}".format(users, int(time.time()))
+#users = comanageService.getRegisteredUsers()
+#registeredUsersMetric = "aai_registered_users_total {0} {1}".format(users, int(time.time()))
 logins = proxystatisticsService.getUsersLogins()
 totalLoginsMetric = "aai_logins_total {0} {1}".format(logins, int(time.time()))
 api_requests = nginxlogsService.getApiRequests()
 totalApiRequestsMetric = "aai_api_requests_total {0} {1}".format(api_requests, int(time.time()))
 f = open("aai_metrics.txt", "w")
-f.write("# TYPE aai_registered_users_total gauge\n")
-f.write("{0}\n".format(registeredUsersMetric))
+#f.write("# TYPE aai_registered_users_total gauge\n")
+#f.write("{0}\n".format(registeredUsersMetric))
 f.write("# TYPE aai_logins_total counter\n")
 f.write("{0}\n".format(totalLoginsMetric))
 f.write("# TYPE aai_api_requests_total counter\n")
